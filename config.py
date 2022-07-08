2c836bf58197f72b04200bfb0fab2c0e1648a194
Hilocv <101907782+Hilocv@users.noreply.github.com>
2022-07-08 12:22:22 -0400

    Add files via upload

---
+++ config.py
@@ -0,0 +1,16 @@
+BOT_TOKEN = '5434037054:AAGpIWy0IeHpqtTOjJSf_lPJgsFRdBcdQKw' #token de tu bot
+API_ID = 14681595 # lo creas en https://my.telegram.org/
+API_HASH = 'a86730aab5c59953c424abb4396d32d5'  # lo creas en https://my.telegram.org/
+OWNER = [1759969205]
+CHAT_ID = '-1001666279733'
+
+# DATOS DEL MOODLE
+USUARIO = 'Lokodas'
+MOODLE_PASSWORD = 'Yeni@#1212'
+USUARIO_ID = 19816 #Tu id
+ZIP_MB = 399
+MOODLE_URL = 'https://moodle.uclv.edu.cu/'
+
+# CUENTA DE MEGA - este bot sirve par mega tambien
+GMAIL_MEGA = 'AQUI TU GMAIL' 
+PASS_MEGA = 'AQUI TU PASS'
